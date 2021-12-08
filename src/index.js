573e76f1292fe116d2ffa6a77d8b39ca20eedf97
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-12-08 16:58:44 +0500

    ready task-5-3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./css/main.css";
 import "./sass/index.scss";
+import myImage from "./img/download.jpeg";
 
 ReactDom.render(
     <div>
+      <img src={myImage} alt="Password" />
       <h3>
         Hello World just React in Webpack
       </h3>
--- webpack.config.js
+++ webpack.config.js
@@ -31,17 +31,6 @@ module.exports = {
   module : {
     rules : [
       {
-         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-      ]
-      },
-      {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
@@ -51,6 +40,31 @@ module.exports = {
           },
         },
       },
+      {
+        test : /\.css$/,
+        use : [
+          'style-loader', 'css-loader',
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader", 
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+      ]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ]
+      }
     ],
   }
 }
